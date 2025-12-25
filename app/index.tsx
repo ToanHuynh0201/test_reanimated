6eac6ef29d7f54fdbf66d4675766aafe471730f3
Huỳnh Nhật Toàn <huynhatoan0201@gmail.com>
2025-12-25 09:18:01 +0700

    refactor: update animation logic and styles for improved responsiveness

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,74 @@
 import React from "react";
-import { Button, StyleSheet, View } from "react-native";
+import { Button, Dimensions, StyleSheet, View } from "react-native";
 import Animated, { useSharedValue, withSpring } from "react-native-reanimated";
+const { width } = Dimensions.get("screen");
+const BOX_SIZE = width / 4;
+const STEP = 50;
 const index = () => {
-	const width = useSharedValue(100);
+	const translateX = useSharedValue(-width / 2 + BOX_SIZE / 2);
+	const direction = useSharedValue(1); // 1 = sang phải, -1 = sang trái
 	const handlePress = () => {
-		width.value = withSpring(Math.random() * 100 + 50);
+		const nextX = translateX.value + STEP * direction.value;
+
+		const rightLimit = width / 2 - BOX_SIZE / 2;
+		const leftLimit = -rightLimit;
+
+		if (nextX > rightLimit || nextX < leftLimit) {
+			// Đảo hướng
+			direction.value *= -1;
+			translateX.value = withSpring(
+				translateX.value + STEP * direction.value,
+			);
+		} else {
+			translateX.value = withSpring(nextX);
+		}
 	};
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-				gap: 50,
-			}}>
+		<View style={styles.container}>
 			<Animated.View
-				style={{
-					width,
-					height: 100,
-					borderRadius: 15,
-					backgroundColor: "violet",
-				}}
+				style={[
+					styles.box,
+					{ transform: [{ translateX: translateX }] },
+				]}
 			/>
-
 			<Button
 				onPress={handlePress}
 				title="Press me!"
 			/>
 		</View>
 	);
+	// const translateX = useSharedValue(0);
+
+	// const handlePress = () => {
+	// 	translateX.value = withSpring(translateX.value + 50);
+	// };
+
+	// return (
+	// 	<View style={styles.container}>
+	// 		<Animated.View
+	// 			style={[styles.box, { transform: [{ translateX }] }]}
+	// 		/>
+	// 		<Button
+	// 			onPress={handlePress}
+	// 			title="Click me"
+	// 		/>
+	// 	</View>
+	// );
 };
 
 export default index;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 20,
+	},
+	box: {
+		width: BOX_SIZE,
+		height: 100,
+		borderRadius: 15,
+		backgroundColor: "violet",
+	},
+});
